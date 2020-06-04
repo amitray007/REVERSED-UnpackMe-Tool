3deeb9f9cca5d3578b48088fcde67828118175a4
Amit Ray <amitrayvii@gmail.com>
2020-06-04 18:10:06 +0530

    Create UnpackMe.cs

---
+++ Unpacked/UnpackMe.cs
@@ -0,0 +1,230 @@
+using System;
+
+namespace Unpackme
+{
+	// Token: 0x02000002 RID: 2
+	internal class BlockingTrimStart
+	{
+		// Token: 0x06000004 RID: 4 RVA: 0x00009004 File Offset: 0x00007204
+		private static void setNInameRawSecurityDescriptor(string[] args)
+		{
+			int num = 0;
+			while (num != 4)
+			{
+				int num2;
+				int[] array;
+				object obj;
+				string text;
+				bool flag;
+				calli(System.Void(System.Int32&,System.Int32&,System.Int32[]&,System.Object&,System.String&,System.Boolean&), ref num, ref num2, ref array, ref obj, ref text, ref flag, BlockingTrimStart.CollectFromServerContextgetContextProperties[num]);
+			}
+		}
+
+		// Token: 0x06000005 RID: 5 RVA: 0x00009034 File Offset: 0x00007234
+		public BlockingTrimStart()
+		{
+			int num = 4;
+			while (num != 0)
+			{
+				int num2;
+				int[] array;
+				calli(System.Void(System.Int32&,System.Int32&,System.Int32[]&,Unpackme.BlockingTrimStart), ref num, ref num2, ref array, this, BlockingTrimStart.CollectFromServerContextgetContextProperties[num]);
+			}
+		}
+
+		// Token: 0x06000006 RID: 6 RVA: 0x00009060 File Offset: 0x00007260
+		private static void AwaitTaskContinuationGray(ref int A_0, ref int A_1, ref int[] A_2, ref object A_3, ref string A_4, ref bool A_5)
+		{
+			object obj;
+			char[] value = obj = new char[8];
+			A_3 = obj;
+			A_3[0] = (1743982953 ^ 1743982908);
+			A_3[1] = (1905425859 ^ 1905425837);
+			A_3[2] = (982540412 ^ 982540300);
+			A_3[3] = (850904451 ^ 850904546);
+			A_3[4] = (2105414317 ^ 2105414350);
+			A_3[5] = (1635163219 ^ 1635163192);
+			A_3[6] = (600346261 ^ 600346360);
+			A_3[7] = (1783492011 ^ 1783492046);
+			Console.Title = new string(value);
+			Console.ForegroundColor = ConsoleColor.White;
+			char[] value2 = obj = new char[14];
+			A_3 = obj;
+			A_3[0] = (955837654 ^ 955837574);
+			A_3[1] = (628985903 ^ 628985934);
+			A_3[2] = (1950710986 ^ 1950710969);
+			A_3[3] = (1042058840 ^ 1042058795);
+			A_3[4] = (37600996 ^ 37600915);
+			A_3[5] = (317480279 ^ 317480248);
+			A_3[6] = (60253712 ^ 60253794);
+			A_3[7] = (1608005769 ^ 1608005869);
+			A_3[8] = (1081253457 ^ 1081253489);
+			A_3[9] = (959759575 ^ 959759541);
+			A_3[10] = (1921389873 ^ 1921389891);
+			A_3[11] = (985707961 ^ 985707990);
+			A_3[12] = (1634889598 ^ 1634889540);
+			A_3[13] = (2140619059 ^ 2140619027);
+			Console.WriteLine(new string(value2));
+			string text = Console.ReadLine();
+			A_4 = text;
+			string a = A_4;
+			char[] value3 = obj = new char[36];
+			A_3 = obj;
+			A_3[0] = (1120623480 ^ 1120623413);
+			A_3[1] = (722681416 ^ 722681349);
+			A_3[2] = (2089955168 ^ 2089955130);
+			A_3[3] = (76841483 ^ 76841541);
+			A_3[4] = (1865469015 ^ 1865468954);
+			A_3[5] = (770545459 ^ 770545513);
+			A_3[6] = (465255347 ^ 465255421);
+			A_3[7] = (955895360 ^ 955895338);
+			A_3[8] = (710428303 ^ 710428412);
+			A_3[9] = (674736877 ^ 674736768);
+			A_3[10] = (57137899 ^ 57137829);
+			A_3[11] = (1615691762 ^ 1615691699);
+			A_3[12] = (560177190 ^ 560177182);
+			A_3[13] = (1752747664 ^ 1752747687);
+			A_3[14] = (1935368235 ^ 1935368211);
+			A_3[15] = (1441387553 ^ 1441387536);
+			A_3[16] = (892378817 ^ 892378872);
+			A_3[17] = (287833245 ^ 287833263);
+			A_3[18] = (489995021 ^ 489995040);
+			A_3[19] = (1830783404 ^ 1830783424);
+			A_3[20] = (1794953111 ^ 1794953208);
+			A_3[21] = (2087792387 ^ 2087792495);
+			A_3[22] = (1246857103 ^ 1246857122);
+			A_3[23] = (84456620 ^ 84456597);
+			A_3[24] = (1388431498 ^ 1388431547);
+			A_3[25] = (1493819134 ^ 1493819078);
+			A_3[26] = (1041566337 ^ 1041566387);
+			A_3[27] = (1582529861 ^ 1582529800);
+			A_3[28] = (1460055005 ^ 1460054963);
+			A_3[29] = (1477609338 ^ 1477609216);
+			A_3[30] = (272811015 ^ 272811113);
+			A_3[31] = (1111053530 ^ 1111053481);
+			A_3[32] = (1264139516 ^ 1264139460);
+			A_3[33] = (1750847541 ^ 1750847588);
+			A_3[34] = (784802379 ^ 784802428);
+			A_3[35] = (259858996 ^ 259858956);
+			bool flag = a == new string(value3);
+			A_5 = flag;
+			int num = (!A_5 * true + true) ? 1 : 0;
+			A_0 = num;
+		}
+
+		// Token: 0x06000007 RID: 7 RVA: 0x0000972C File Offset: 0x0000792C
+		private static void ResourceManagerLookupStartedDefinitionToTextBuffer(ref int A_0, ref int A_1, ref int[] A_2, ref object A_3, ref string A_4, ref bool A_5)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			object obj;
+			char[] value = obj = new char[16];
+			A_3 = obj;
+			A_3[0] = (908432930 ^ 908432965);
+			A_3[1] = (2028572834 ^ 2028572877);
+			A_3[2] = (1917177851 ^ 1917177748);
+			A_3[3] = (1977284449 ^ 1977284366);
+			A_3[4] = (1318236664 ^ 1318236567);
+			A_3[5] = (2147425941 ^ 2147426033);
+			A_3[6] = (2066041247 ^ 2066041279);
+			A_3[7] = (1275974109 ^ 1275974071);
+			A_3[8] = (984920941 ^ 984920834);
+			A_3[9] = (569392881 ^ 569392787);
+			A_3[10] = (1904786736 ^ 1904786704);
+			A_3[11] = (454989695 ^ 454989585);
+			A_3[12] = (1820121181 ^ 1820121140);
+			A_3[13] = (1787349551 ^ 1787349576);
+			A_3[14] = (67380758 ^ 67380849);
+			A_3[15] = (1633556628 ^ 1633556725);
+			Console.WriteLine(new string(value));
+			A_0 = 3;
+		}
+
+		// Token: 0x06000008 RID: 8 RVA: 0x00009918 File Offset: 0x00007B18
+		private static void UserDataBuiltInPermissionFlag(ref int A_0, ref int A_1, ref int[] A_2, ref object A_3, ref string A_4, ref bool A_5)
+		{
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			object obj;
+			char[] value = obj = new char[16];
+			A_3 = obj;
+			A_3[0] = (495712940 ^ 495712965);
+			A_3[1] = (1951589841 ^ 1951589823);
+			A_3[2] = (345665948 ^ 345666047);
+			A_3[3] = (1180314740 ^ 1180314651);
+			A_3[4] = (1623307960 ^ 1623307978);
+			A_3[5] = (2005498548 ^ 2005498566);
+			A_3[6] = (835893632 ^ 835893733);
+			A_3[7] = (371649881 ^ 371649850);
+			A_3[8] = (1876462769 ^ 1876462789);
+			A_3[9] = (1030209133 ^ 1030209101);
+			A_3[10] = (1643324002 ^ 1643323918);
+			A_3[11] = (1380302612 ^ 1380302713);
+			A_3[12] = (401925862 ^ 401925771);
+			A_3[13] = (1093568016 ^ 1093568113);
+			A_3[14] = (351552246 ^ 351552153);
+			A_3[15] = (956813296 ^ 956813306);
+			Console.WriteLine(new string(value));
+			Console.ForegroundColor = ConsoleColor.White;
+			char[] value2 = obj = new char[21];
+			A_3 = obj;
+			A_3[0] = (967945285 ^ 967945237);
+			A_3[1] = (1675509239 ^ 1675509125);
+			A_3[2] = (532954623 ^ 532954522);
+			A_3[3] = (1011289630 ^ 1011289709);
+			A_3[4] = (518139041 ^ 518139090);
+			A_3[5] = (660194219 ^ 660194187);
+			A_3[6] = (512546740 ^ 512546773);
+			A_3[7] = (1795010817 ^ 1795010927);
+			A_3[8] = (21751139 ^ 21751066);
+			A_3[9] = (2118366641 ^ 2118366609);
+			A_3[10] = (1247019326 ^ 1247019349);
+			A_3[11] = (819038617 ^ 819038716);
+			A_3[12] = (1736516045 ^ 1736516020);
+			A_3[13] = (586576642 ^ 586576674);
+			A_3[14] = (1909892327 ^ 1909892243);
+			A_3[15] = (1820189101 ^ 1820189122);
+			A_3[16] = (1410228580 ^ 1410228548);
+			A_3[17] = (786738034 ^ 786737943);
+			A_3[18] = (615340590 ^ 615340630);
+			A_3[19] = (1460033322 ^ 1460033347);
+			A_3[20] = (1405181593 ^ 1405181677);
+			Console.WriteLine(new string(value2));
+			Console.ReadLine();
+			A_0 = 4;
+		}
+
+		// Token: 0x06000009 RID: 9 RVA: 0x00009D60 File Offset: 0x00007F60
+		private static void HistoryMetadataToken(ref int A_0, ref int A_1, ref int[] A_2, ref object A_3, ref string A_4, ref bool A_5)
+		{
+			Console.ReadLine();
+			A_0 = 4;
+		}
+
+		// Token: 0x0600000A RID: 10 RVA: 0x00009D80 File Offset: 0x00007F80
+		private static void ReferenceToTextBufferInvokeMethod(ref int A_0, ref int A_1, ref int[] A_2, BlockingTrimStart A_3)
+		{
+			A_3..ctor();
+			A_0 = 0;
+		}
+
+		// Token: 0x0600000B RID: 11 RVA: 0x00009DA4 File Offset: 0x00007FA4
+		// Note: this type is marked as 'beforefieldinit'.
+		static BlockingTrimStart()
+		{
+			BlockingTrimStart.ObjectWriterXDQ();
+		}
+
+		// Token: 0x0600000C RID: 12 RVA: 0x00009DB8 File Offset: 0x00007FB8
+		private static void ObjectWriterXDQ()
+		{
+			BlockingTrimStart.CollectFromServerContextgetContextProperties = new IntPtr[5];
+			BlockingTrimStart.CollectFromServerContextgetContextProperties[0] = ldftn(AwaitTaskContinuationGray);
+			BlockingTrimStart.CollectFromServerContextgetContextProperties[1] = ldftn(ResourceManagerLookupStartedDefinitionToTextBuffer);
+			BlockingTrimStart.CollectFromServerContextgetContextProperties[2] = ldftn(UserDataBuiltInPermissionFlag);
+			BlockingTrimStart.CollectFromServerContextgetContextProperties[3] = ldftn(HistoryMetadataToken);
+			BlockingTrimStart.CollectFromServerContextgetContextProperties[4] = ldftn(ReferenceToTextBufferInvokeMethod);
+		}
+
+		// Token: 0x04000002 RID: 2
+		private static IntPtr[] CollectFromServerContextgetContextProperties;
+	}
+}
